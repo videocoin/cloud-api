01cb7c931e189e4ea8388f929bb6edfe1bb7db81
earl <earl@liveplanet.net>
2019-04-22 16:01:33 -0700

    Update jobs

--- workorder/v1/workorder.pb.validate.go
+++ workorder/v1/workorder.pb.validate.go
@@ -214,3 +214,88 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = JobProfileValidationError{}
+
+// Validate checks the field values on JobProfiles with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *JobProfiles) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	for idx, item := range m.GetJobProfiles() {
+		_, _ = idx, item
+
+		{
+			tmp := item
+
+			if v, ok := interface{}(tmp).(interface{ Validate() error }); ok {
+
+				if err := v.Validate(); err != nil {
+					return JobProfilesValidationError{
+						field:  fmt.Sprintf("JobProfiles[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+		}
+
+	}
+
+	return nil
+}
+
+// JobProfilesValidationError is the validation error returned by
+// JobProfiles.Validate if the designated constraints aren't met.
+type JobProfilesValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e JobProfilesValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e JobProfilesValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e JobProfilesValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e JobProfilesValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e JobProfilesValidationError) ErrorName() string { return "JobProfilesValidationError" }
+
+// Error satisfies the builtin error interface
+func (e JobProfilesValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sJobProfiles.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = JobProfilesValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = JobProfilesValidationError{}
