be48d8510b2b69d70bd9b866939264b84f4af62e
earl <earl@liveplanet.net>
2019-04-23 10:36:51 -0700

    Remove client_address from jobRequest

--- manager/v1/manager.pb.validate.go
+++ manager/v1/manager.pb.validate.go
@@ -858,8 +858,6 @@ func (m *JobRequest) Validate() error {
 
 	// no validation rules for PipelineId
 
-	// no validation rules for ClientAddress
-
 	if _, ok := v1.ProfileId_name[int32(m.GetProfileId())]; !ok {
 		return JobRequestValidationError{
 			field:  "ProfileId",
