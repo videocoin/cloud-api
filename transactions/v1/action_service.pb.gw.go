67fc912ab7cf609a062ffa97c6e8eb63c27ed98f
Jonathan Gimeno <jgimeno@gmail.com>
2019-07-05 10:46:53 +0200

    rename endpoints

--- transactions/v1/action_service.pb.gw.go
+++ transactions/v1/action_service.pb.gw.go
@@ -172,9 +172,9 @@ func RegisterActionServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 }
 
 var (
-	pattern_ActionService_GetActions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 2}, []string{"api", "v1", "address"}, ""))
+	pattern_ActionService_GetActions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "actions", "address"}, ""))
 
-	pattern_ActionService_GetAction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "transaction", "hash"}, ""))
+	pattern_ActionService_GetAction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v1", "action", "hash"}, ""))
 )
 
 var (
