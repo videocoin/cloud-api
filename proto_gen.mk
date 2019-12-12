210e6e6860f1d083f3ef64fe1b477211374a3626
cyc60 <avsysoev60@gmail.com>
2019-12-13 00:52:19 +0300

    Fix proto build

--- proto_gen.mk
+++ proto_gen.mk
@@ -39,6 +39,7 @@ protoc-v1-%:
 	protoc \
 		-I/usr/local/include \
 		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway \
 		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
 		-I${GOPATH}/src/github.com \
 		-I. \
